5e7b7a16a92855b49439bde0520834adb9ca2419
Saeid Saberi <s.saberi@yahoo.com>
2019-08-19 11:10:19 +0430

    Update .eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    "env": {
+        "node": true,
+        "browser": true,
+        "react-native/react-native": true,
+        "es6": true,
+        "jest": true
+    },
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-native"
+    ],
+    "rules": {
+      "comma-dangle": "off",
+      "global-require": "off",
+      "import/no-extraneous-dependencies": "off",
+      "import/no-named-as-default": "off",
+      "object-curly-newline": "off",
+      "quotes": [2, "double"],
+      "react/jsx-filename-extension": "off",
+      "react/prop-types": "off",
+      "react/jsx-one-expression-per-line": "off"
+    }
 };
